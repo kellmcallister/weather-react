b0e0780d883ba9e21ec77803096e6ea1c44fd6e7
kellmcallister <128011075+kellmcallister@users.noreply.github.com>
2023-06-13 14:48:55 -0500

    Removed unnecessary images and edited header

--- src/App.js
+++ src/App.js
@@ -5,28 +5,32 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div className="container">
-          <h1>✨Explore Weather✨</h1>
-          <div className="card">
-            <div className="card-body">
-              <div className="search-row">
-                <Search />
-              </div>
+        <h1>✨Explore Weather✨</h1>{" "}
+      </header>{" "}
+      <div className="container">
+        <div className="card">
+          <div className="card-body">
+            <div className="search-row">
+              <Search />
             </div>
           </div>
         </div>
-        <div className="github-link">
-          Coded by Kelly McAllister - check out my projects on
-          <a
-            href="https://github.com/kellmcallister"
-            target="_blank"
-            rel="noreferrer"
-          >
-            {" "}
-            GitHub!
-          </a>
-        </div>
-      </header>
+      </div>
+      <div className="github-link">
+        Coded by{" "}
+        <a href="https://www.kellmcallister.com" target="_blank">
+          Kelly McAllister
+        </a>{" "}
+        - check out my projects on
+        <a
+          href="https://github.com/kellmcallister"
+          target="_blank"
+          rel="noreferrer"
+        >
+          {" "}
+          GitHub!
+        </a>
+      </div>
     </div>
   );
 }
